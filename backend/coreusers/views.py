e759e3c95b6148f0fdd16ac942e12998985fbd07
Julian steven arias sierra <jariass2@ucentral.edu.co>
2025-11-06 23:06:09 -0500

    Cambios en front y en back, front se mejoro la visual de la pantalla principal, en back se mejoro el momento que se asignan usuarios

--- backend/coreusers/views.py
+++ backend/coreusers/views.py
@@ -6,6 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import PatientAssignment, User
 from .permissions import IsAdminUserRole
